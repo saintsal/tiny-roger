f2b22f3d65776e1ec7fb608d5f141257b430f877
wighawag <wighawag@gmail.com>
2023-02-19 13:34:31 +0000

    new tab format for nav

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -4,12 +4,12 @@
 	import ThemeChanger from '$lib/components/website/themes/ThemeChanger.svelte';
 </script>
 
-<div class="navbar bg-base-100 min-h-0 p-1">
+<div class="navbar bg-base-100 min-h-0 p-1 border-b-2">
 	<div class="flex-1">
-		<div class="tabs">
-			<NavElement class="tab tab-bordered" selected="tab-active" href="/">Home</NavElement>
-			<NavElement class="tab tab-bordered" selected="tab-active" href="/demo/">Demo</NavElement>
-			<NavElement class="tab tab-bordered" selected="tab-active" href="/test/">Test</NavElement>
+		<div class="tabs tabs-boxed bg-base-100">
+			<NavElement class="tab" selected="tab-active" href="/">Home</NavElement>
+			<NavElement class="tab" selected="tab-active" href="/demo/">Demo</NavElement>
+			<NavElement class="tab" selected="tab-active" href="/test/">Test</NavElement>
 		</div>
 	</div>
 	<div class="flex-none">
