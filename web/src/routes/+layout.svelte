a8dd9cf6fa9446fc8ed14549dcf418cede12cfbc
wighawag <wighawag@gmail.com>
2023-02-19 17:59:16 +0000

    sticky header

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import NavTabs from '$lib/components/daisyui/NavTabs.svelte';
 </script>
 
-<div class="navbar bg-base-100 min-h-0 p-1 border-b-2 border-primary">
+<div class="sticky top-0 z-50 navbar bg-base-100 min-h-0 p-1 border-b-2 border-primary">
 	<div class="flex-1">
 		<NavTabs
 			pages={[
