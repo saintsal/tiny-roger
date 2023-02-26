01c783339c469c60824e5e0d4704345425ae37b9
wighawag <wighawag@gmail.com>
2023-02-26 16:08:11 +0000

    setup

--- contracts/src/Tokens.sol
+++ contracts/src/Tokens.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: AGPL-1.0
 pragma solidity 0.8.17;
 
-import "solidity-kit/src/ERC20/implementations/ERC20Base.sol";
-import "solidity-kit/src/ERC20/ERC2612/implementations/UsingPermitWithDynamicChainId.sol";
+import "solidity-kit/solc_0.8/ERC20/implementations/ERC20Base.sol";
+import "solidity-kit/solc_0.8/ERC20/ERC2612/implementations/UsingPermitWithDynamicChainID.sol";
 
-contract Tokens is ERC20Base, UsingPermitWithDynamicChainId {
-	constructor(address to, uint256 amount) UsingPermitWithDynamicChainId() {
+contract Tokens is ERC20Base, UsingPermitWithDynamicChainID {
+	constructor(address to, uint256 amount) UsingPermitWithDynamicChainID(address(0)) {
 		_mint(to, amount);
 	}
 
 	string public constant symbol = "TOKENS";
 
-	function name() public pure override returns (string memory) {
+	function name() public pure override(Named, IERC20) returns (string memory) {
 		return "Tokens";
 	}
 }
